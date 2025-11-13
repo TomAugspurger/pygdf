ceaaa875880fb8d97f3f4fcf434e416ba28f2e5f
Tom Augspurger <toaugspurger@nvidia.com>
2025-11-13 13:07:36 -0800

    fixup

--- python/cudf_polars/cudf_polars/experimental/rapidsmpf/core.py
+++ python/cudf_polars/cudf_polars/experimental/rapidsmpf/core.py
@@ -13,9 +13,9 @@ from rapidsmpf.buffer.resource import BufferResource, LimitAvailableMemory
 from rapidsmpf.communicator.single import new_communicator
 from rapidsmpf.config import Options, get_environment_variables
 from rapidsmpf.rmm_resource_adaptor import RmmResourceAdaptor
+from rapidsmpf.statistics import Statistics
 from rapidsmpf.streaming.core.context import Context
 from rapidsmpf.streaming.core.leaf_node import pull_from_channel
-from rapidsmpf.statistics import Statistics
 from rapidsmpf.streaming.core.node import (
     run_streaming_pipeline,
 )
@@ -114,9 +114,14 @@ def evaluate_logical_plan(
     else:
         stream_pool = None
 
-    stats = Statistics(enable=True, mr=mr)
-    br = BufferResource(mr, memory_available=memory_available, stream_pool=stream_pool, statistics=stats)
-    rmpf_context = Context(comm, br, options, statistics=stats)
+    rmpf_stats = Statistics(enable=True, mr=mr)
+    br = BufferResource(
+        mr,
+        memory_available=memory_available,
+        stream_pool=stream_pool,
+        statistics=rmpf_stats,
+    )
+    rmpf_context = Context(comm, br, options, statistics=rmpf_stats)
 
     executor = ThreadPoolExecutor(max_workers=1, thread_name_prefix="cpse")
 
@@ -171,7 +176,7 @@ def evaluate_logical_plan(
     # Now we need to drop *all* GPU data. This ensures that no cudaFreeAsync runs
     # before the Context, which ultimately contains the rmm MR, goes out of scope.
     del nodes, output, messages, chunks, dfs, df
-    print(stats.report())
+    print(rmpf_stats.report())
 
     return result
 
